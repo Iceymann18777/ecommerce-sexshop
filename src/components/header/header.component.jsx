1f6edfe6cedc8456e4be464b6daf6732d1fee285
Adam Khader <redrum691@protonmail.com>
2022-02-11 11:08:58 +1100

    Initial commit

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles';
+
+const Header = ({ currentUser, hidden, signOutStart }) => {
+    return(
+        <HeaderContainer>
+            <LogoContainer to="/">
+                <Logo className='logo' />
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to='/shop'>
+                    SHOP
+                </OptionLink>
+                <OptionLink to='/shop'>
+                    CONTACT
+                </OptionLink>
+                {
+                    currentUser ?
+                    <OptionDiv onClick={signOutStart}>SIGN OUT</OptionDiv>
+                    :
+                    <OptionLink to='/signin'>SIGN IN</OptionLink>
+                }
+                <CartIcon />
+            </OptionsContainer>
+            {
+                hidden ? null : <CartDropdown />
+            }
+        </HeaderContainer>
+    )
+}
+
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+});
+
+const mapDispatchToProps = dispatch => ({
+    signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
+
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+
+// import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+// Utilise our HOC WithSpinner to create the enhanced components
+// const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+// const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+const ShopPage = ({ match, fetchCollectionsStart }) => {
+
+    useEffect(() => {
+        fetchCollectionsStart();
+    }, [fetchCollectionsStart]);
+    
+    // componentDidMount() {
+
+    //     fetchCollectionsStart();
+    //     /*
+    //     const { updateCollections } = this.props;
+    //     const collectionRef = firestore.collection('collections');
+    //     */ 
+    //     // Observer Pattern Data Fetching
+    //     // Dynamically updates as changes on the database occur
+    //     /* this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    //         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //         updateCollections(collectionsMap);
+    //         this.setState({ loading: false });
+    //     }); */
+
+    //     // Promise-based Pattern Data Fetching
+    //     // This will limit the data fetching to this single point in time
+    //     // One-off API call
+    //     /* collectionRef.get().then(snapshot => {
+    //         const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //         updateCollections(collectionsMap);
+    //         this.setState({ loading: false });
+    //     }); */
+
+    //     // Promise-based II Pattern Data Fetching (with fetch() function)
+    //     // The conversion of the object received would be different
+    //     /* fetch('https://firestore.googleapis.com/v1/projects/sexygenarias-db/databases/(default)/documents')
+    //     .then(response => response.json())
+    //     .then(collections => console.log(collections)); */
+    // } 
+    
+    return(
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`}
+                component={CollectionsOverviewContainer}
+            />
+            <Route path={`${match.path}/:collectionId`}
+                component={CollectionPageContainer}
+            />
+        </div>
+    );
+}
+
+const mapDispatchToProps = dispatch => ({
+    fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
